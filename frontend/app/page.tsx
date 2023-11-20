0159be7f5cd3cfc1cecf2a1ac142039ed46bc3b6
hisamitsu <koron.take@gmail.com>
2023-11-20 10:42:05 +0900

    クエリの実装まで

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,3 +1,30 @@
-export default function Home() {
+import { getClient } from "@/lib/apolloClient";
+import { gql } from "@apollo/client";
+
+const queryTest = gql`
+  query getMessages {
+    messages {
+      id
+      text
+    }
+  }
+`;
+
+export default async function Home() {
+  // クエリ
+  const { data } = await getClient().query({
+    query: queryTest,
+    context: {
+      fetchOptions: {
+        next: { revalidate: 5 },
+      },
+    },
+  });
+  console.log("クエリ");
+  console.log(data);
+  console.log("----------");
+  // ミューテーション
+
+  // サブスクライブ
   return <main>Hello World!!</main>;
 }
