b2493ac02648b3eb4ce0ab82cbbb7139ef3f222b
hisamitsu <koron.take@gmail.com>
2023-11-20 11:02:19 +0900

    ミューテーションの実装まで

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,25 +5,57 @@ const queryTest = gql`
   query getMessages {
     messages {
       id
+      user
       text
+      createdAt
+    }
+  }
+`;
+
+const mutationTest = gql`
+  mutation postMessage($user: String!, $text: String!) {
+    postMessage(user: $user, text: $text) {
+      id
+      user
+      text
+      createdAt
+    }
+  }
+`;
+
+const subscribeTest = gql`
+  subscription subscribeMessage {
+    messagePosted(user: "hisa") {
+      id
+      user
+      text
+      createdAt
     }
   }
 `;
 
 export default async function Home() {
   // クエリ
-  const { data } = await getClient().query({
+  const { data: queryData } = await getClient().query({
     query: queryTest,
     context: {
       fetchOptions: {
-        next: { revalidate: 5 },
+        next: { revalidate: 1 },
       },
     },
   });
   console.log("クエリ");
-  console.log(data);
+  console.log(queryData);
   console.log("----------");
+
   // ミューテーション
+  const { data: mutationData } = await getClient().mutate({
+    mutation: mutationTest,
+    variables: { user: "hisa1", text: "text1" },
+  });
+  console.log("ミューテーション");
+  console.log(mutationData);
+  console.log("----------");
 
   // サブスクライブ
   return <main>Hello World!!</main>;
