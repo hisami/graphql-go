a646d0c4a66fccdc9ed8514d9a220495bcceeafb
hisamitsu <koron.take@gmail.com>
2023-11-20 21:42:38 +0900

    リファクタリング

--- frontend/app/query/page.tsx
+++ frontend/app/query/page.tsx
@@ -1,64 +1,29 @@
 import { getClient } from "@/lib/apolloClient";
-import { gql } from "@apollo/client";
-import { GetMessagesQuery } from "@/graphql/dist/client";
-
-const queryTest = gql`
-  query getMessages {
-    messages {
-      id
-      user
-      text
-      createdAt
-    }
-  }
-`;
-
-const mutationTest = gql`
-  mutation postMessage($user: String!, $text: String!) {
-    postMessage(user: $user, text: $text) {
-      id
-      user
-      text
-      createdAt
-    }
-  }
-`;
-
-const subscribeTest = gql`
-  subscription subscribeMessage {
-    messagePosted(user: "hisa") {
-      id
-      user
-      text
-      createdAt
-    }
-  }
-`;
+import {
+  GetMessagesQuery,
+  GetMessagesDocument,
+  PostMessageDocument,
+} from "@/graphql/dist/client";
 
 export default async function Home() {
   // クエリ
   const { data: queryData } = await getClient().query<GetMessagesQuery>({
-    query: queryTest,
+    query: GetMessagesDocument,
     // context: {
     //   fetchOptions: {
     //     next: { revalidate: 100 },
     //   },
     // },
   });
-  console.log("クエリ");
-  console.log(queryData);
-  console.log("----------");
 
   // ミューテーション
   // const { data: mutationData } = await getClient().mutate({
-  //   mutation: mutationTest,
-  //   variables: { user: "hisa1", text: "text1" },
+  //   mutation: PostMessageDocument,
+  //   variables: { user: "hisamitsu", text: "text6" },
   // });
   // console.log("ミューテーション");
   // console.log(mutationData);
   // console.log("----------");
-
-  // サブスクライブ
 
   const messages = [...queryData.messages];
   return (
